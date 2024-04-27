c9a147303c580671b9b6630cdefd1973cb2d3260
iwasa-kosui <dev@kosui.me>
2024-04-28 02:06:20 +0900

    chore: add name

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     monkey({
       entry: "src/main.ts",
       userscript: {
+        name: "Gikopoipoi PiP on ChatLog",
         icon: "https://gikopoipoi.net/characters/naito/front-standing.svg",
         namespace: "npm/vite-plugin-monkey",
         match: ["https://gikopoipoi.net/*"],
